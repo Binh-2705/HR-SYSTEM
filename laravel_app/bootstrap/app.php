63da0dc4f490520f19b881c89b1242b2a412d7f3
Tuannt25 <thaituan43214321@gmail.com>
2026-07-02 23:13:43 +0700

    dunggg

--- laravel_app/bootstrap/app.php
+++ laravel_app/bootstrap/app.php
@@ -6,7 +6,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-
+//dsakjdjsak
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
