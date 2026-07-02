b08f5bdc3ca1a4b49b849df27818b0a3e5d8f52a
truonggamo <truonggi2005@gmail.com>
2026-07-02 23:12:03 +0700

    truong test xung đột

--- laravel_app/bootstrap/app.php
+++ laravel_app/bootstrap/app.php
@@ -6,7 +6,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-
+//quan ly service container
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
